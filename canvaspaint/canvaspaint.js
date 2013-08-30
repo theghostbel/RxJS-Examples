50d1b7b9e4a36d6bc7c1249aa1e4903c3170666d
Victor Rusakovich <nemiga@gmail.com>
2013-08-30 23:39:03 +0300

    Return earlier to remove extra indentation

--- canvaspaint/canvaspaint.js
+++ canvaspaint/canvaspaint.js
@@ -10,32 +10,32 @@
     function main() {
         var canvas = document.querySelector('#canvas');
 
-        if (canvas.getContext) {
-            var ctx = canvas.getContext('2d');
-            ctx.beginPath();
+        if (!canvas.getContext) { return; }
+        
+        var ctx = canvas.getContext('2d');
+        ctx.beginPath();
 
-            // Get mouse moves
-            var mouseMoves = Rx.Observable.fromEvent(canvas, 'mousemove');
+        // Get mouse moves
+        var mouseMoves = Rx.Observable.fromEvent(canvas, 'mousemove');
 
-            // Calculate difference between two mouse moves
-            var mouseDiffs = mouseMoves.bufferWithCount(2, 1).select(function (x) {
-                return { first: getOffset(x[0]), second: getOffset(x[1]) };
-            });
-            
-            // Get merge together both mouse up and mouse down
-            var mouseButton = Rx.Observable.fromEvent(canvas, 'mousedown').select(function (x) { return true; })
-                .merge(Rx.Observable.fromEvent(canvas, 'mouseup').select(function (x) { return false; }));
+        // Calculate difference between two mouse moves
+        var mouseDiffs = mouseMoves.bufferWithCount(2, 1).select(function (x) {
+            return { first: getOffset(x[0]), second: getOffset(x[1]) };
+        });
+        
+        // Get merge together both mouse up and mouse down
+        var mouseButton = Rx.Observable.fromEvent(canvas, 'mousedown').select(function (x) { return true; })
+            .merge(Rx.Observable.fromEvent(canvas, 'mouseup').select(function (x) { return false; }));
 
-            // Paint if the mouse is down
-            var paint = mouseButton.select(function (down) { return down ? mouseDiffs : mouseDiffs.take(0) }).switchLatest();
+        // Paint if the mouse is down
+        var paint = mouseButton.select(function (down) { return down ? mouseDiffs : mouseDiffs.take(0) }).switchLatest();
 
-            // Update the canvas
-            paint.subscribe(function (x) {
-                ctx.moveTo(x.first.offsetX, x.first.offsetY);
-                ctx.lineTo(x.second.offsetX, x.second.offsetY);
-                ctx.stroke();
-            });
-        }
+        // Update the canvas
+        paint.subscribe(function (x) {
+            ctx.moveTo(x.first.offsetX, x.first.offsetY);
+            ctx.lineTo(x.second.offsetX, x.second.offsetY);
+            ctx.stroke();
+        });
     }
 
     main();
